7fa9851db70be55895e8b79a0b4c7f5325efa703
sawanee10 <sawanee4010@gmail.com>
2024-10-19 23:21:47 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer py-3 bg-dark text-white text-center mt-3">
       <div className="container">
-        &copy; 2024 Jakraphong Jantarupan
+        &copy; 2024 sawanee 
       </div>
     </footer>
   );
